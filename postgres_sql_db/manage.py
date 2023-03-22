240c37d2e78f36e272223fa563713d73a29199cb
sbhat <sbhat@deqode.com>
2023-03-22 11:10:52 +0530

    7

--- postgres_sql_db/manage.py
+++ postgres_sql_db/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#hellojkljjljhhl
+#hellojkljjljhhlhlhlkhjlkjhlkj
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'postgres_sql_db.settings')
